533e9dd71b5867b9d72735e18e8ce4aac085df16
Fazal Kareem <fkareem@hercules11.bc.rzg.mpg.de>
2024-10-08 05:05:38 +0200

    pics classifier v2.0 code added

---
+++ pics_classifier_v2.0.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python2.7
+
+import os, sys
+import glob
+import cPickle
+import argparse
+import pandas as pd
+import logging
+sys.path.append('/home/psr')
+from ubc_AI.data import pfdreader
+import subprocess, errno
+
+# Set up logging for verbose output
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def score_file(filename, classifier_model):
+    try:
+        return classifier_model.report_score([pfdreader(filename)])
+    except Exception as e:
+        logging.error("Error scoring file %s: %s" % (filename, e))
+        return 0
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+def run_pics_parallel(filenames, pics_model, model_name):
+    logging.info("Running parallel scoring for model %s..." % model_name)
+    classifier_model = cPickle.load(open(pics_model,'rb'))
+    AI_scores = classifier_model.report_score([pfdreader(f) for f in filenames])
+    df = pd.DataFrame({'filename': filenames, model_name: AI_scores})
+    return df
+
+def run_pics_sequential(filenames, pics_model, model_name):
+    logging.info("Running sequential scoring for model %s..." % model_name)
+    classifier_model = cPickle.load(open(pics_model, 'rb'))
+    AI_scores = []
+    for f in filenames:
+        try:
+            score = classifier_model.report_score([pfdreader(f)])
+            AI_scores.append(score)
+        except Exception as e:
+            logging.error("Error scoring file %s: %s" % (f, e))
+            AI_scores.append(0)
+    df = pd.DataFrame({'filename': filenames, model_name: AI_scores})
+    return df
+
+def main(input_dir, pics_model_dir):
+    logging.info("Starting processing in directory %s" % input_dir)
+    os.chdir(input_dir)
+    filenames = glob.glob('*.pfd') + glob.glob('*.ar') + glob.glob('*.ar2')
+    logging.info("Found %d files to process" % len(filenames))
+    master_df = pd.DataFrame({'filename': filenames})
+    models = glob.glob(pics_model_dir + '/' + '*.pkl')
+    logging.info("Found %d models to apply" % len(models))
+
+    for pics_model in models:
+        model_name = os.path.splitext(os.path.basename(pics_model))[0]
+        try:
+            df = run_pics_parallel(filenames, pics_model, model_name)
+        except Exception as e:
+            logging.error("Parallel scoring failed for model %s, falling back to sequential scoring: %s" % (model_name, e))
+            df = run_pics_sequential(filenames, pics_model, model_name)
+        
+        # Sort individual model DataFrame
+        df = df.sort_values(by=[model_name], ascending=False)
+        
+        # Merge into the master DataFrame
+        master_df = pd.merge(master_df, df, on='filename', how='left')
+        
+    # Save the master DataFrame
+    output_file = os.path.join(input_dir, 'pics_scores.csv')
+    master_df.to_csv(output_file, index=False)
+    logging.info("Scoring completed. Results saved to %s" % output_file)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process a directory of files and output to a CSV file.')
+    parser.add_argument('-i', '--input_dir', required=True, help='Input dir of files to process')
+    parser.add_argument('-m', '--model_dir', required=True, help='PICS model directory used to score')
+
+    args = parser.parse_args()
+    input_dir = args.input_dir
+    pics_model_dir = args.model_dir
+    
+    main(input_dir, pics_model_dir)
