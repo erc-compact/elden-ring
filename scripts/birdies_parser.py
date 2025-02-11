2ec5a345f8c8482c7daba2351bd490e8d979637f
wintervolcano <fkareem@mpifr-bonn.mog.de>
2025-02-12 00:54:50 +0100

    segmented search

---
+++ scripts/birdies_parser.py
@@ -0,0 +1,56 @@
+import xml.etree.ElementTree as ET
+import argparse
+
+def extract_frequencies(xml_file):
+    """
+    Extracts periods from the XML file and calculates frequencies (1/period).
+
+    Args:
+        xml_file (str): The path to the XML file.
+        fft_size (int): The size of the FFT to compute the frequency bin spacing.
+
+    Returns:
+        List of tuples: List containing frequencies in Hz and the corresponding 1/fft_size values.
+    """
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    # Find the period information
+    periods = []
+    for i, candidate in enumerate(root.findall('.//candidate')):
+        period = float(candidate.find('period').text)  # Assuming period tag exists
+        frequencies = 1.0 / period  # Frequency is the inverse of the period
+        periods.append(frequencies)
+
+    tsamp = float(root.find('.//tsamp').text)
+    nsamples = int(root.find('.//nsamples').text)
+    
+    # Calculate bin spacing upto 4 decimal places
+    bin_spacing = float(1.0 / (2 * tsamp * nsamples))  # Nyquist frequency is 1/(2 * tsamp)
+
+    # Prepare the list of frequencies with the corresponding bin spacing
+    freq_bin_pairs = [(freq, bin_spacing) for freq in periods]
+
+    return freq_bin_pairs
+
+def write_birdies_to_file(frequencies):
+    """
+    Write the frequencies and their corresponding 1/fft_size values to a file.
+
+    Args:
+        frequencies (list): List of tuples with frequency and bin spacing.
+        output_file (str): The file to write the frequencies to.
+    """
+    with open("birdies.txt", 'w') as f:
+        for freq, spacing in frequencies:
+            f.write(f"{freq:.2f} {spacing:.3f}\n")
+
+def main(args):
+    frequencies = extract_frequencies(args.xml_file)
+    write_birdies_to_file(frequencies)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Extract periods from an XML file, calculate frequencies, and save them in a specific format.")
+    parser.add_argument('--xml_file', type=str, help='Path to the XML file.')
+    args = parser.parse_args()
+    main(args)
---
+++ scripts/fold_cands_to_csv.py
@@ -0,0 +1,58 @@
+import argparse
+import logging
+import os
+import pandas as pd
+import xml.etree.ElementTree as ET
+from bestprof_utils import parse_bestprof
+import subprocess
+import sys
+
+def a_to_pdot(P_s, acc_ms2):
+    LIGHT_SPEED = 2.99792458e8  # Speed of Light in SI
+    return P_s * acc_ms2 / LIGHT_SPEED
+
+def calculate_spin(f=None, fdot=None, p=None, pdot=None):
+    if f is not None and fdot is not None:
+        p = 1 / f
+        pdot = -fdot / (f ** 2)
+    elif p is not None and pdot is not None:
+        f = 1 / p
+        fdot = -pdot * (p ** 2)
+    else:
+        raise ValueError("Either (f, fdot) or (p, pdot) must be provided")
+    return f, fdot, p, pdot
+
+def pulsarx_to_csv_format(filenames, pulsarx_cand_file, output_file):
+    df = pd.read_csv(pulsarx_cand_file, skiprows=11, sep='\s+')
+    df['fold_cands_filename'] = filenames
+
+    f, fdot, p, pdot = calculate_spin(f=df['f0_new'].values, fdot=df['f1_new'].values)
+    df['p0_new'] = p
+    df['p1_new'] = pdot
+    f, fdot, p, pdot = calculate_spin(f=df['f0_old'].values, fdot=df['f1_old'].values)
+    df['p0_old'] = p
+    df['p1_old'] = pdot
+    df = df.astype({
+        "#id": int, "fold_cands_filename": str, "f0_new": float, "f1_new": float,
+        "dm_new": float, "S/N_new": float, "f0_old": float, "f1_old": float,
+        "dm_old": float, "S/N": float, "p0_old": float, "p1_old": float,
+        "p0_new": float, "p1_new": float, "search_candidates_database_uuid": str
+    })
+    
+    df.to_csv(output_file, index=False)
+
+def main():
+    parser = argparse.ArgumentParser(description="Process pulsar data")
+    # parser.add_argument("-p", "--process_name", choices=["pulsarx", "presto"], required=True, help="Name of the process to execute")
+    parser.add_argument("-f", "--filenames", nargs='+', required=True, help="List of filenames to process")
+    parser.add_argument("-c", "--pulsarx_cand_file", help="PulsarX candidate file")
+    parser.add_argument("-o", "--output_file", help="Output file name", default="search_fold_cands.csv")
+
+    args = parser.parse_args()
+    
+    pulsarx_to_csv_format(
+            args.filenames, args.pulsarx_cand_file, args.output_file
+        )
+    
+if __name__ == "__main__":
+    main()
